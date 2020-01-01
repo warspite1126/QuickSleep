9c60ae5346e75ff33c3e87d3bca7ba7ddf7c3277
Ritsuki Goto <develop@mizucoffee.net>
2020-01-01 23:34:23 +0900

    :tada: add project

---
+++ src/main/kotlin/com/example/spigot_boilerplate/Main.kt
@@ -0,0 +1,10 @@
+package com.example.spigot_boilerplate
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+        println("Hello Spigot-Boilerplate!")
+    }
+}
